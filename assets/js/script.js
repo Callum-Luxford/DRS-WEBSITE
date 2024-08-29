c89a65a2b1f474729eab827f8e0fcefc51e4799f
Callum Luxford <151152271+Callum-Luxford@users.noreply.github.com>
2024-08-29 11:08:01 +0100

    Changes to forms, scripts etc

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,15 +90,51 @@ var typed = new Typed(".auto-type", {
 // End Type Writer
 
 // Form Validation
+// Form 1 variables
 const formSubmit = document.getElementById("submit");
-const formNameInput = document.querySelector('input[name="name"]');
-const formPhone = document.querySelector('input[name="tel"]');
-const formEmail = document.querySelector('input[name="email"]');
-const formMessage = document.querySelector('textarea[name="message"]');
+
+const formNameInput = document.querySelector(
+  "form[name='form__1'] input[name='name']"
+);
+const formPhone = document.querySelector(
+  "form[name='form__1'] input[name='tel']"
+);
+const formEmail = document.querySelector(
+  "form[name='form__1'] input[name='email']"
+);
+const formMessage = document.querySelector(
+  "form[name='form__1'] textarea[name='message']"
+);
 const errorMessagesDiv = document.getElementById("error__message");
 
-const inputs = [formNameInput, formPhone, formEmail, formMessage];
+// Form 2 variables
+const form2 = document.getElementById("form__submit2");
 
+const formNameInput2 = document.querySelector(
+  "form[name='form__2'] input[name='name']"
+);
+const formPhone2 = document.querySelector(
+  "form[name='form__2'] input[name='tel']"
+);
+const formEmail2 = document.querySelector(
+  "form[name='form__2'] input[name='email']"
+);
+const formMessage2 = document.querySelector(
+  "form[name='form__2'] textarea[name='message']"
+);
+const errorMessagesDiv2 = document.getElementById("error__message2");
+
+
+// form 1 inputs
+const inputs = [
+  formNameInput,
+  formPhone,
+  formEmail,
+  formMessage,
+];
+
+// form 2 inputs
+const inputs2 = [formNameInput2, formPhone2, formEmail2, formMessage2];
 isFormValid = false;
 
 const removeError = (e) => {
@@ -119,6 +155,17 @@ const removeErrorMessage = (e) => {
   e.classList.remove("error__message");
 };
 
+const addErrorMessage2 = (e) => {
+  errorMessagesDiv2.style.display = "block";
+  e.classList.add("error__message");
+};
+
+const removeErrorMessage2 = (e) => {
+  errorMessagesDiv2.style.display = "none";
+  e.classList.remove("error__message");
+};
+
+// Form 1
 const validateInputs = () => {
   isFormValid = true;
   inputs.forEach(removeError);
@@ -148,16 +195,134 @@ const validateInputs = () => {
     addErrorMessage(errorMessagesDiv);
   }
 };
-
 inputs.forEach((input) => {
   input.addEventListener("input", () => {
     validateInputs();
   });
 });
-
 formSubmit.addEventListener("click", (e) => {
   // e.preventDefault();
   validateInputs();
+
+  // insert captcha here
+
+  const Captcha = form.querySelector(
+    "textarea[name=g-recaptcha-response]"
+  ).value;
+  if (!Captcha) {
+    e.preventDefault();
+    alert("Please fill out captcha field");
+    throw new Error("Captcha not complete");
+    return;
+  }
+
+  // End insert captcha here
+  console.log("here");
+});
+// Validations for the form inside of html
+const url =
+  "https://script.google.com/macros/s/AKfycbwrKmZ3ep-MhICGXa5jnbF9Mmz-EGsn05QHk8hfccxG7aheCAmCeU82qWDaJ7P3tTJu/exec";
+const form = document.getElementById("my__form");
+
+form.addEventListener("submit", submitter);
+function submitter(e) {
+  console.log("submitted");
+  e.preventDefault();
+  let message = "";
+  if (formNameInput.value.length < 3) {
+    message += `<br>- Name needs to be more than 3 characters.`;
+  }
+  if (formEmail.value.length < 5) {
+    message += `<br>- Email needs to be a valid email.`;
+  }
+  if (formPhone.value.length < 11) {
+    message += `<br>- Phone needs to be a valid UK Number.`;
+  }
+  if (formMessage.value.length < 20) {
+    message += `<br>- Message needs to be greater than 20 characters`;
+  }
+  if (message) {
+    const div = document.createElement("div");
+    div.innerHTML = message;
+    div.style.color = "red";
+    div.style.display = "inline";
+    form.prepend(div);
+    setTimeout(() => div.remove(), 5000);
+  } else {
+    const formObj = {
+      name: formNameInput.value,
+      email: formEmail.value,
+      phone: formPhone.value,
+      message: formMessage.value,
+    };
+    addSendMail(formObj);
+    openPopup();
+  }
+}
+// sending the form data.
+function addSendMail(data) {
+  console.log(data);
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+    });
+}
+// retreiving the form data.
+function addSendMailGET(data) {
+  const url1 = url + "?id=100";
+  fetch(url1)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+    });
+}
+
+
+// Form 2
+const validateInputs2 = () => {
+  isFormValid = true;
+  inputs2.forEach(removeError);
+  removeErrorMessage2(errorMessagesDiv2);
+
+  if (!formNameInput2.value) {
+    addError(formNameInput2);
+    isFormValid = false;
+    addErrorMessage2(errorMessagesDiv2);
+  }
+
+  if (!formPhone2.value) {
+    isFormValid = false;
+    addError(formPhone2);
+
+    addErrorMessage2(errorMessagesDiv2);
+  }
+
+  if (!formEmail2.value) {
+    isFormValid = false;
+    addError(formEmail2);
+
+    addErrorMessage2(errorMessagesDiv2);
+  }
+
+  if (!formMessage2.value) {
+    isFormValid = false;
+    addError(formMessage2);
+
+    addErrorMessage2(errorMessagesDiv2);
+  }
+};
+inputs2.forEach((input) => {
+  input.addEventListener("input", () => {
+    validateInputs2();
+  });
+});
+form2.addEventListener("click", (e) => {
+  // e.preventDefault();
+  validateInputs2();
 
   // insert captcha here
 
@@ -190,6 +355,7 @@ function closePopup() {
   blur.classList.remove("blur__container");
   location.reload();
 }
+
 // End Pop Up For User Upon Submission
 
 // Web3form API
@@ -233,91 +399,26 @@ function closePopup() {
 // });
 // End Web3form API
 
-
 // FORM DATA (Version of forms without WEB3)
 
 // Connecting to google app script for POST AND GET and validations.
-const url =
-  "https://script.google.com/macros/s/AKfycbwrKmZ3ep-MhICGXa5jnbF9Mmz-EGsn05QHk8hfccxG7aheCAmCeU82qWDaJ7P3tTJu/exec";
-const form = document.getElementById("my__form");
-
-// Validations for the form inside of html
-form.addEventListener('submit', submitter);
-function submitter(e) {
-  console.log('submitted');
-  e.preventDefault();
-  let message = "";
-  if(formNameInput.value.length < 3) {
-    message += `<br>- Name needs to be more than 3 characters.`;
-  }
-  if(formEmail.value.length < 5) {
-    message += `<br>- Email needs to be a valid email.`;
-  }
-  if (formPhone.value.length < 11) {
-    message += `<br>- Phone needs to be a valid UK Number.`;
-  }
-  if(formMessage.value.length < 20) {
-    message += `<br>- Message needs to be greater than 20 characters`;
-  }
-  if(message) {
-    const div = document.createElement('div');
-    div.innerHTML = message;
-    div.style.color = 'red';
-    div.style.display = 'inline';
-    form.prepend(div);   
-    setTimeout(()=>div.remove(),5000);
-  } else {
-    const formObj = {
-      name:formNameInput.value,
-      email:formEmail.value,
-      phone:formPhone.value,
-      message:formMessage.value,
-    };
-    addSendMail(formObj);
-    openPopup();
-  }
-};
-
-// sending the form data.
-function addSendMail(data){
-  console.log(data);
-  fetch(url,{
-    method: 'POST',
-    body:JSON.stringify(data)
-  })
-  .then(res => res.json())
-  .then(json =>{
-    console.log(json);
-  })
-}
-
-// retreiving the form data.
-function addSendMailGET(data){
-  const url1 = url + '?id=100';
-  fetch(url1)
-  .then(res => res.json())
-  .then(json =>{
-    console.log(json);
-  })
-}
 
 
 // SCALE RECAPTCHA
-function scaleCaptcha(){
+function scaleCaptcha() {
   const captchaWidth = 1000;
   const containerWidth = $(".g-recaptcha").width();
-  if(captchaWidth>containerWidth){
-    var captchaScale = (containerWidth/captchaWidth)*2;
+  if (captchaWidth > containerWidth) {
+    var captchaScale = (containerWidth / captchaWidth) * 2;
     $(".g-recaptcha").css({
-      "transform":`scale(${captchaScale})`
-    })
+      transform: `scale(${captchaScale})`,
+    });
   }
 }
 
-scaleCaptcha()
-$(window).resize( scaleCaptcha );
+scaleCaptcha();
+$(window).resize(scaleCaptcha);
 // End SCALE RECAPTCHA
-
 
 // SWIPER JS
 const swiper = new Swiper(".swiper", {
@@ -348,10 +449,8 @@ const swiper = new Swiper(".swiper", {
 
     340: {
       slidesPerView: 2,
-      spaceBetween: 30
+      spaceBetween: 30,
     },
   },
 });
 // SWIPER JS
-
-
